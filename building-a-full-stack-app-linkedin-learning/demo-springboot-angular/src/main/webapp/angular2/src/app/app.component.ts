a343205a016652a9b28da084249fb7b7f9e7a402
Sayan Guha <sayanguha99@gmail.com>
2020-06-26 19:51:01 +0530

    Created webapp components which will be hooked to REST API

--- building-a-full-stack-app-linkedin-learning/demo-springboot-angular/src/main/webapp/angular2/src/app/app.component.ts
+++ building-a-full-stack-app-linkedin-learning/demo-springboot-angular/src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular2';
+export class AppComponent implements OnInit {
+
+	public submitted: boolean;
+	roomsearch: FormGroup;
+	rooms: Room[];
+
+	ngOnInit() {
+		this.roomsearch = new FormGroup({
+			checkin: new FormControl(''),
+			checkout: new FormControl('')
+		});
+		
+		this.rooms=ROOMS;
+	}
+
+	onSubmit({ value, valid}: { value: Roomsearch, valid: boolean }) {
+		console.log(value);
+	}
+	
+	reserveRoom(value:string){
+		console.log("Room id:"+value);
+	}
 }
+export interface Roomsearch {
+	checkin: string;
+	checkout: string;
+}
+
+export interface Room {
+	id: string;
+	roomNumber: string;
+	price: string;
+	links:string;
+}
+
+/**The array below simulates a response from REST API */
+var ROOMS:Room[]=[
+	{
+		"id": "35446565",
+		roomNumber:"101",
+		price:"2000",
+		links:""		
+	},
+	{
+		"id": "45446565",
+		roomNumber:"102",
+		price:"2000",
+		links:""		
+	},
+	{
+		"id": "55446565",
+		roomNumber:"103",
+		price:"2100",
+		links:""		
+	}
+]

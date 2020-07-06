944ef33e4e88a7717914658f9a9da7f18958a64a
Sayan Guha <sayanguha99@gmail.com>
2020-07-06 18:49:58 +0530

    First Commit for new application practice

---
+++ building-a-full-stack-app-linkedin-learning/demo-springboot-microservices/src/main/java/com/sayan/microservices/demospringbootmicroservices/DemoSpringbootMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.microservices.demospringbootmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootMicroservicesApplication.class, args);
+	}
+
+}
---
+++ building-a-full-stack-app-linkedin-learning/demo-springboot-microservices/src/test/java/com/sayan/microservices/demospringbootmicroservices/DemoSpringbootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.microservices.demospringbootmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

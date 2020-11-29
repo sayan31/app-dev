37986be3384c101d55a34a3aaa30d7c434fd0df7
Sayan Guha <sayanguha99@gmail.com>
2020-11-29 14:53:35 +0530

    Add few images and styling changes

--- building-a-full-stack-app-linkedin-learning/demo-springboot-microservices/src/main/demo-springboot-microservices-client/src/app/components/book/book.component.ts
+++ building-a-full-stack-app-linkedin-learning/demo-springboot-microservices/src/main/demo-springboot-microservices-client/src/app/components/book/book.component.ts
@@ -20,6 +20,12 @@ export class BookComponent implements OnInit{
       this.path='/assets/the-predators-ball.jpg';
     }else if (this.book.isbn==35618906){
       this.path='/assets/annapurna.jpg';
+    }else if (this.book.isbn==9781324002642){
+      this.path='/assets/the-fifth-risk.jpg';
+    }else if (this.book.isbn==9780393351590){
+      this.path='/assets/flash-boys.jpg';
+    }else if (this.book.isbn==9780140446036){
+      this.path='/assets/arthashastra.jpg';
     }
   }
 
